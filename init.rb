62da336751b608fd06ba7cdf49388ec348419cc6
Eric Davis <edavis@littlestreamsoftware.com>
2010-07-19 14:24:42 -0700

    Added plugin docs.

--- init.rb
+++ init.rb
@@ -1,10 +1,12 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_remove_membership do
-  name 'Redmine Remove Membership plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  name 'Remove Membership'
+  author 'Eric Davis'
+  description 'Adds a Rake task to remove all memberships for a specific user'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-misc'
+  author_url 'http://www.littlestreamsoftware.com'
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.9.0'
 end
